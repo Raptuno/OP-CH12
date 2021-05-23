b88a8bbabb1eefd3777407721b8b116119ced9a5
Raptuno <65292196+Raptuno@users.noreply.github.com>
2021-05-23 11:10:58 -0500

    Al fin le entendí

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-try:
-    x = float(input("Escriba el valor de x: "))
-except ValueError:
-    x = float(0)
-
-try:
-    y = float(input("Escriba el valor de y: "))
-except ValueError:
-    y = float(0)
-
-try:
-    vx = float(input("Escriba la velocidad de x: "))
-except ValueError:
-    vx = float(1)
-
-try:
-    vy = float(input("Escriba la velocidad de y: "))
-except ValueError:
-    vy = float(1)
-
-
 class Particula:
     _x: float
     _y: float
@@ -26,16 +5,44 @@ class Particula:
     _vy: float
 
     def __init__(self):
-        self._x = x
-        self._y = y
-        self._vx = vx
-        self._vy = vy
+        try:
+            self._x = float(input("Escriba la coordenada x: "))
+        except ValueError:
+            self._x = float(0)
+        try:
+            self._y = float(input("Escriba la coordenada y: "))
+        except ValueError:
+            self._y = float(0)
+        try:
+            self._vx = float(input("Escriba la velocidad en el eje x: "))
+        except ValueError:
+            self._vx = float(1)
+        try:
+            self._vy = float(input("Escriba la velocidad en el eje y: "))
+        except ValueError:
+            self._vy = float(1)
 
-    #def dt(self):
-     #   return
-    # def _mover(dt):
+    def dt(self):
+        # Coordenadas
+        cx = self._x
+        cy = self._y
+        # Desplazamiento
+        dpx = self._x + self._vx
+        dpy = self._y + self._vy
+        return cx, cy, dpx, dpy
+
+    def _mover(self, tupler):
+        returns = tupler
+
+        cx = returns[0]
+        cy = returns[1]
+        desX = returns[2]
+        desY = returns[3]
+
+        print(f"Partícula en posición ({cx}, {cy}) y velocidad ({desX}, {desY})")
 
 
 ptc = Particula
 
 ptc.__init__(ptc)
+ptc._mover(ptc, ptc.dt(ptc))
